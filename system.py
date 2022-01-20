b6c32da52b8369304aea3a9d30f4b1ae9fbf9723
DelinM <delinmuray@gmail.com>
2022-01-19 23:23:41 -0500

    small modification

--- system.py
+++ system.py
@@ -1,10 +1,13 @@
 import os
 
+
+def get_currentpath():
+    return os.getcwd()
+
+
 def get_opsystem():
     op_system = os.name
-    if op_system =='posix':
+    if op_system == 'posix':
         return 'Unix'
     else:
         return 'Windows'
-
-print(get_opsystem())
