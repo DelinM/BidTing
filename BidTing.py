aae2ddc9be482d6464c6405894151e61a4e31118
DelinM <delinmuray@gmail.com>
2022-01-22 22:20:14 -0500

    update init

--- BidTing.py
+++ BidTing.py
@@ -63,6 +63,7 @@ def click_button_awarded(driver):
 
     time.sleep(5)
 
+
 def click_button_page100(driver):
     from selenium.webdriver.support.ui import WebDriverWait
     try:
@@ -76,10 +77,39 @@ def click_button_page100(driver):
 
     time.sleep(10)
 
+
 def get_driverpagenumber(driver):
     page_number = driver.find_element(by=By.CLASS_NAME, value='repeater-pages')
     print(page_number)
     page_number = int(page_number.text)
-    return page_number
+    if page_number >= 0:
+        return page_number
+    else:
+        raise Exception('Error: Page number cannot be extracted (get_driverpagenumber)')
 
 
+def click_button_nextpage(driver):
+    button_next = driver.find_element(by=By.CSS_SELECTOR, value='.btn.btn-default.repeater-next')
+    button_next.click()
+
+
+def get_projects_basiccontainer(driver):
+    from bs4 import BeautifulSoup as soup
+    html = driver.page_source
+    soup_projects = soup(html, 'html5lib')
+    project_container = soup_projects.find_all('tbody')[0]
+    project_container_inner = project_container.find_all('div', {'style': 'float:right;'})
+    return project_container_inner
+
+
+def get_projects_basicinformation(basiccontainer, url, clientname, list_projectname, list_projectweb, list_clientname):
+    for project in basiccontainer:
+        project_name = project.span.contents[0]
+        project_name = project_name.split('- ')[2].strip()
+
+        # receive the url to get into the page
+        project_website = url + project.a['href']
+        list_projectname.append(project_name)
+        list_projectweb.append(project_website)
+        list_clientname.append(clientname)
+        print(project_website)
--- init.py
+++ init.py
@@ -6,17 +6,42 @@ import time
 start = time.time()
 webdriver = 'firefox'
 url = 'https://york.bidsandtenders.ca/Module/Tenders/en'
+client_name = 'York Region'
+
 
 path = bt.get_path_webdriver(webdriver)
 driver = bt.get_driver(path)
 driver.get(url)
 bt.click_button_awarded(driver)
 bt.click_button_page100(driver)
+page_number = bt.get_driverpagenumber(driver)
 
 list_projects = dm.init_list_projects()
 list_submitters = dm.init_list_projectsubmitters()
 
+time.sleep(10)
 
+page_start = 0
+
+for i in range(page_start, page_number, 1):
+    time.sleep(30)
+
+    '''below included if page_start is not 0, for future implementation'''
+    if i == page_start and page_start != 0:
+        time.sleep(20)
+
+
+
+    list_projectname = []
+    list_projectweb = []
+    list_clientname = []
+
+    project_container = bt.get_projects_basiccontainer(driver)
+    bt.get_projects_basicinformation(project_container,url,client_name,list_projectname,list_projectweb, list_clientname)
+
+    if i < page_number - 1:
+        bt.click_button_nextpage(driver)
+driver.quit()
 
 end = time.time()
 print('Run time is', end - start)
