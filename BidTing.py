8b05a6d904bc82b0c3a765f82ffab54e3a652c1b
DelinM <delinmuray@gmail.com>
2022-03-19 16:43:18 -0400

    update init_ios

--- BidTing.py
+++ BidTing.py
@@ -1,5 +1,4 @@
 import os.path
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,34 +13,44 @@ def get_path_webdriver(driver):
     driver = driver.lower()
 
     path = sa.get_currentpath()
-    if sa.get_Windows() and driver == 'chrome':
-        chromedriver_name = 'chromedriver.exe'
-        path = os.path.join(path, chromedriver_name)
-        return path
-    elif sa.get_Windows() and driver == 'firefox':
-        firefoxdriver_name = 'geckodriver.exe'
-        path = os.path.join(path, firefoxdriver_name)
-        return path
+
+    if sa.get_Windows():
+        if driver == 'chrome':
+            chromedriver_name = 'chromedriver.exe'
+            path = os.path.join(path, chromedriver_name)
+            return path
+        elif driver == 'firefox':
+            firefoxdriver_name = 'geckodriver.exe'
+            path = os.path.join(path, firefoxdriver_name)
+            return path
+    elif not sa.get_Windows():
+        if driver == 'chrome':
+            chromedriver_name = 'chromedriver'
+            path = os.path.join(path, chromedriver_name)
+            return path
+        elif driver == 'firefox':
+            firefoxdriver_name = 'geckodriver'
+            path = os.path.join(path, firefoxdriver_name)
+            return path
     else:
-        raise Exception('ERROR: The system is not windows.')
+        raise Exception('Error: unable to get webdriver (path issue)')
 
 
-def get_driver(path):
-    if 'chromedriver.exe' in path:
-
+def get_driver(path, headless):
+    if 'chromedriver.exe' or 'chromedriver' in path:
         from selenium.webdriver.chrome.service import Service
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        if headless:
+            options.add_argument('--headless')
         options.add_argument('--incognito')
         options.add_argument('--ignore_certificate-error')
         chr_service = Service(path)
         driver = webdriver.Chrome(service=chr_service, options=options)
         return driver
 
-    elif 'geckodriver.exe' in path:
+    elif 'geckodriver.exe' or 'geckodriver' in path:
 
         from selenium.webdriver.firefox.service import Service
-
         firef_service = Service(path)
         driver = webdriver.Firefox(service=firef_service)
         return driver
@@ -113,3 +122,274 @@ def get_projects_basicinformation(basiccontainer, url, clientname, list_projectn
         list_projectweb.append(project_website)
         list_clientname.append(clientname)
         print(project_website)
+
+def get_projects(url, driver, page_number, page_start, client_name):
+    list_projectname = []
+    list_projectweb = []
+    list_clientname = []
+
+    for i in range(page_start, page_number, 1):
+        '''time buffer added, to be removed.'''
+        time.sleep(10)
+
+        '''below included if page_start is not 0, for future implementation'''
+        if i == page_start and page_start != 0:
+            time.sleep(10)
+
+        project_container = get_projects_basiccontainer(driver)
+        get_projects_basicinformation(project_container, url, client_name, list_projectname, list_projectweb,
+                                         list_clientname)
+
+        if i < page_number - 1:
+            click_button_nextpage(driver)
+
+    driver.quit()
+
+    return (list_projectname, list_projectweb, list_clientname)
+
+
+def get_project_biddinginformation(project_name, client_name, url):
+    '''
+    start development: Feb 19th, 2021
+    end development: ?????
+
+    Purpose: extract a AWARDED project information when a AWARDED project url is provided. The function will return
+    a list of AWARDED project information.
+
+
+    Patch 0.1 - Feb 25th, 221
+    Some awarded project does not have award table or submitted project table, so codes to be modified so that
+    if some projects do not have the table(s), the code will return empty
+
+    Patch 0.2 - March 6th, 2021
+    Changes to make to extract Bid Number. Bid Number is important as it will be act as an "ID" for each project to
+    prevent duplications in future database.
+
+    Patch 0.3 - March 6th, 2021
+    Some projects do not have bid classification, code added to avoid if everything we look for are blank.
+    Multiple bugs were fixed under this patch
+    1. When opened page is an 'Error' page, the code can now identify and will return an empty list
+    2. When awarded table is missing, the code can now identify and will return an empty winner list
+
+    Patch 0.4 - Wining Price Extraction - March 14th, 2021
+    Code modification to extract awarded company' cost.
+
+    Patch 0.5 - Precision Extraction - March 14th, 2021
+    Code modification to "precision" extract the information we need.
+    1. Before information were extract based on the location, but now information will be extracted base on keywords.
+    2. Before project name and project type were extract from Function "BidTenders_AwardProjects", which was not ideal
+       as the name show in the main page is not accurate. Modification is made.
+
+    Patch 0.6 - Bids Submitted Information Extraction - March 15th, 2021
+    Codes added to extract Bids Submitted Information
+
+    Patch 0.7 - Bid Cost Update - March 16th, 2021
+    cCode patch to solve the following problems.
+    1. Sometimes, the winners in the submitter table has a project cost, but the cost is not in the winner's table, and
+    vice versa.
+    2. list_submitter can supersede the list_project
+
+
+    '''
+    from bs4 import BeautifulSoup as soup
+
+
+    # receive HTML using selenium chrome
+    webdriver = 'chrome'
+    path = get_path_webdriver(webdriver)
+    driver = get_driver(path, headless=False)
+    driver.get(url)
+    time.sleep(0)
+    html = driver.page_source
+    driver.close()
+
+    soup_ob = soup(html, 'html.parser')
+
+    # web checking:
+    error = str(soup_ob.find_all('body')[0].contents[0])
+    if 'error' in error:
+        return [client_name, project_name,'','','','','','','','','','']
+
+    else:
+
+        # code below are modified during Patch 0.5, project_information contains all necessary information
+        project_information_list = soup_ob.find_all('div', {'id': 'pnlBidDetails_Container'})
+        project_information = project_information_list[0]
+
+        # receive bid classification - Patch 0.5 completed
+        bid_classification = project_information.find_all('div', {'id': 'bidClass'})
+        if len(bid_classification) == 0:
+            output_bid_classification = ''
+        else:
+            output_bid_classification = bid_classification[0]
+            output_bid_classification = output_bid_classification.contents[0]
+
+
+        # for loop to extract bid type, bid ID, bid name, bid date, and bid year
+        project_infobox = project_information.find_all('td')
+
+        # set initial value for all output values
+        output_bid_type = ''
+        output_bid_id = ''
+        output_projectname = ''
+        output_awarded_date = ''
+        output_awarded_year = ''
+        for p in range(len(project_infobox)):
+
+            if len(project_infobox[p].contents) != 0:
+
+                if 'Bid Type' in project_infobox[p].contents[0]:
+                    output_bid_type = project_infobox[p+1].contents[0].strip()
+
+
+                elif 'Bid Number' in project_infobox[p].contents[0]:
+                    output_bid_id = project_infobox[p+1].contents[0].strip()
+
+
+                elif 'Bid Name' in project_infobox[p].contents[0]:
+                    output_projectname = project_infobox[p+1].contents[0].strip().title()
+
+
+                elif 'Awarded Date' in project_infobox[p].contents[0]:
+                    awarded_time = project_infobox[p+1].contents[0].split()
+                    output_awarded_date = awarded_time[1] + ' ' + awarded_time[2].rstrip(',')
+                    output_awarded_year = awarded_time[3]
+
+                elif output_awarded_date =='' and output_awarded_year == '' and 'Bid Closing Date' in project_infobox[p].contents[0]:
+                    awarded_time = project_infobox[p + 1].contents[0].split()
+                    output_awarded_date = awarded_time[1] + ' ' + awarded_time[2].rstrip(',')
+                    output_awarded_year = awarded_time[3]
+
+        # receive awarded winners' name, they will be organzied in a string, and return how many winners
+        # if there is one single winner, receive winner's price.
+
+        awarded_company_table = soup_ob.find_all('div', {'id': 'dgAwarded_Container'})
+        if len(awarded_company_table) == 0:
+            output_no_winner = 0
+            output_winnercompany_name =''
+            output_winnerprice = ''
+
+        else:
+            awarded_company_table = awarded_company_table[0]
+            awarded_company_names = awarded_company_table.find_all('div',
+                                                                   {'class': 'x-grid3-cell-inner x-grid3-col-CompanyName'})
+            output_no_winner = 0
+            output_winnercompany_name = ''
+            for company_name in awarded_company_names:
+                company_name = company_name.contents[0].strip()
+                output_no_winner += 1
+                output_winnercompany_name += company_name + ', '
+                output_winnerprice = ''
+
+            # statement in place in case the awarded company is not recorded.
+            if output_winnercompany_name == '':
+                output_no_winner = 0
+                output_winnerprice = ''
+
+            if output_no_winner == 1:
+                output_winnerprice = awarded_company_table.find_all('div',
+                                                                   {'class': 'x-grid3-cell-inner x-grid3-col-Value'})
+                output_winnerprice = output_winnerprice[0].contents[0]
+
+
+        # receive plan takers' names, they will be organized in a string, and return how many takers
+
+        # Patch 0.6: add list to store submitter's information: [bid_id, bid_company, results]
+        list_submitter = []
+        output_winnerstatus = 0
+        submitted_company_table = soup_ob.find_all('div', {'id': 'dgSubmitted_Container'})
+        if len(submitted_company_table) == 0:
+            output_no_submitted = 0
+            output_submitted_name = ''
+            list_submitter.append([client_name,
+                                   output_projectname,
+                                   output_bid_classification,
+                                   output_bid_type,
+                                   output_bid_id,
+                                   output_awarded_date,
+                                   output_awarded_year,
+                                   '',
+                                   '',
+                                   ''])
+        else:
+            submitted_company_table = submitted_company_table[0]
+            submitted_company_names = submitted_company_table.find_all('div', {
+                'class': 'x-grid3-cell-inner x-grid3-col-CompanyName'})
+            submitted_company_price = submitted_company_table.find_all('div', {
+                'class': 'x-grid3-cell-inner x-grid3-col-VerifiedValue'})
+
+
+            output_no_submitted = 0
+            output_submitted_name = ''
+
+            # patch: code modified to fix unstable price issue
+            list_submitted_price = []
+            for element in submitted_company_price:
+                if len(element.contents) > 1:
+                    sum = ''
+                    for value in element:
+                        sum += str(value)
+                    if '<br/>' in sum:
+                        sum = sum.replace('<br/>','')
+                    list_submitted_price.append(sum.strip())
+                else:
+                    list_submitted_price.append(element.contents[0].strip())
+
+            for position, company_name in enumerate(submitted_company_names):
+
+                # receive company name and company price
+                company_name = company_name.contents[0].strip()
+                company_price = list_submitted_price[position].strip()
+
+                if company_price == '--':
+                    company_price = ''
+
+                # if the company is a winner, but the winner's price is not updated, then use the data in submitter
+                # table to update, and vice versa (patch 0.7)
+                list_awarded_company_names = output_winnercompany_name.split(', ')
+                if len(list_awarded_company_names) == 1:
+                    if list_awarded_company_names[0] == company_name:
+                        output_winnerstatus = 1
+                        if output_winnerprice == '--' or output_winnerprice == '':
+                            output_winnerprice = company_price
+                        elif company_price == '--' or company_price == '':
+                            company_price = output_winnerprice
+                if len(list_awarded_company_names) != 1:
+                    if company_name in list_awarded_company_names:
+                        output_winnerstatus = 1
+                # Patch 0.6: write into submitter's list
+                list_submitter.append([client_name,
+                                       output_projectname,
+                                       output_bid_classification,
+                                       output_bid_type,
+                                       output_bid_id,
+                                       output_awarded_date,
+                                       output_awarded_year,
+                                       company_name,
+                                       company_price,
+                                       output_winnerstatus])
+                output_winnerstatus = 0
+                # write into project information spreadsheet
+                output_no_submitted += 1
+                output_submitted_name += company_name + ', '
+
+            output_winnercompany_name = output_winnercompany_name.rstrip(', ')
+            output_submitted_name = output_submitted_name.rstrip(', ')
+
+        list_project = [client_name,
+                        output_projectname,
+                        output_bid_classification,
+                        output_bid_type,
+                        output_bid_id,
+                        output_awarded_date,
+                        output_awarded_year,
+                        output_no_winner,
+                        output_winnercompany_name,
+                        output_winnerprice,
+                        output_no_submitted,
+                        output_submitted_name]
+
+        project_list = [list_project,list_submitter]
+
+        return project_list
+
--- init_mac.py
+++ init_mac.py
@@ -1,20 +1,50 @@
 import BidTing as bt
 import datam as dm
 import time
-
+import os
+import system_access as sa
 
 start = time.time()
 
-webdriver = 'firefox'
+webdriver = 'chrome'
 url = 'https://haltonregion.bidsandtenders.ca/'
 client_name = 'Halton Region'
 
-
+print('client_name')
 ''' Computer Program '''
 
 
+# Receive initial url
+path = bt.get_path_webdriver(webdriver)
+driver = bt.get_driver(path, headless=False)
+driver.get(url)
+
+# Receive page numbers need to be scraped
+bt.click_button_awarded(driver)
+bt.click_button_page100(driver)
+page_number = bt.get_driverpagenumber(driver)
+
+# forcesleep
+time.sleep(10)
+page_start = 0
+
+
+# split out a tuple: ((list_projectname, list_projectweb, list_clientname))
+projects_info = bt.get_projects(url, driver, page_number, page_start, client_name)
+
+
+n = len(projects_info[0])
+
+for i in range(n):
+    project_list = bt.get_project_biddinginformation(projects_info[0][i],
+                                      client_name,
+                                      projects_info[1][i])
+
+    print(project_list)
 
 
 end = time.time()
+print('Run time is', end - start)
 
-print('total process time:{} second', end-start)
+print(projects_info[0])
+print(len(projects_info[0]))
